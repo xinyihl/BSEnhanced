0049244b3159ef8b00ba77aba608ae5a592f13fd
xinyihl <1012737146@qq.com>
2023-02-14 22:31:08 +0800

    Update PluginConstants.java

--- src/main/java/cn/xinyihl/constants/PluginConstants.java
+++ src/main/java/cn/xinyihl/constants/PluginConstants.java
@@ -3,7 +3,7 @@ package cn.xinyihl.constants;
 import cn.xinyihl.util.ConfigUtil;
 
 public class PluginConstants {
-    public final static String PLUGIN_VERSION_URL = "https://github.com/api/v5/repos/Server-CT/BSEnhanced/releases/latest";
+    public final static String PLUGIN_VERSION_URL = "https://api.github.com/repos/xinyihl/BSEnhanced/releases/latest";
 
     public final static String PLUGIN_API_URL = ConfigUtil.CONFIG.getString("BSE-API");
 
